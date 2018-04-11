8fa9fa7e92b8f113c0077553e01dde2910545759
Diego Borrero <die.9511@gmail.com>
2018-04-10 20:09:48 -0500

    Keyboard function added, Animation added

--- src/main/resources/static/js/KleptomaniaGameController.js
+++ src/main/resources/static/js/KleptomaniaGameController.js
@@ -1,6 +1,8 @@
 var model = {
     thisxpos:0,
     thisypos:0,
+    thisvx:0,
+    thisvy:0,
     loadedPolices:[],
     loadedObjects:[],
     loadedThiefs:[]
@@ -16,37 +18,33 @@ var GameModelModule = (function () {
     var TextureCache = PIXI.utils.TextureCache;
     var Sprite = PIXI.Sprite;
     var Rectangle = PIXI.Rectangle;
+
+    //Sprites var
     var PRStatic;
-    var PRMove1;
-    var PRMove2;
     var PLStatic;
-    var PLMove1;
-    var PLMove2;
     var PUStatic;
-    var PUMove1;
-    var PUMove2;
     var PDStatic;
-    var PDMove1;
-    var PDMove2;
     var TRStatic;
-    var TRMove1;
-    var TRMove2;
     var TLStatic;
-    var TLMove1;
-    var TLMove2;
     var TUStatic;
-    var TUMove1;
-    var TUMove2;
     var TDStatic;
-    var TDMove1;
-    var TDMove2;
     var Diamond;
     var Coin;
     var Gold;
+    var PUpAnim;
+    var PDownAnim;
+    var PLeftAnim;
+    var PRightAnim;
+    var TUpAnim;
+    var TDownAnim;
+    var TLeftAnim;
+    var TRightAnim;
 
+    //Util var
+    var animationType = 0;
+    var animationDir;
+    var SpriteAct;
 
-
-    
     //Initial setup
     var setupPixiApp = function () {
         window.addEventListener("resize", function() {
@@ -61,93 +59,298 @@ var GameModelModule = (function () {
         loader
             .add("spritesheet","../images/Spritesheet.json")
             .load( function () {
-                console.log(resources.spritesheet.textures);
                 PRStatic = new Sprite(resources.spritesheet.textures["PRStatic.png"]);
+                PRStatic.scale.x = 0.3;
+                PRStatic.scale.y = 0.3;
                 PRStatic.visible = false;
                 app.stage.addChild(PRStatic);
-                PRMove1 = new Sprite(resources.spritesheet.textures["PRMov1.png"]);
-                PRMove1.visible = false;
-                app.stage.addChild(PRMove1);
-                PRMove2 = new Sprite(resources.spritesheet.textures["PRMov2.png"]);
-                PRMove2.visible = false;
-                app.stage.addChild(PRMove2);
                 PLStatic = new Sprite(resources.spritesheet.textures["PLStatic.png"]);
+                PLStatic.scale.x = 0.3;
+                PLStatic.scale.y = 0.3;
                 PLStatic.visible = false;
                 app.stage.addChild(PLStatic);
-                PLMove1 = new Sprite(resources.spritesheet.textures["PLMov1.png"]);
-                PLMove1.visible = false;
-                app.stage.addChild(PLMove1);
-                PLMove2 = new Sprite(resources.spritesheet.textures["PLMov2.png"]);
-                PLMove2.visible = false;
-                app.stage.addChild(PLMove2);
                 PUStatic = new Sprite(resources.spritesheet.textures["PUStatic.png"]);
+                PUStatic.scale.x = 0.3;
+                PUStatic.scale.y = 0.3;
                 PUStatic.visible = false;
                 app.stage.addChild(PUStatic);
-                PUMove1 = new Sprite(resources.spritesheet.textures["PUMov1.png"]);
-                PUMove1.visible = false;
-                app.stage.addChild(PUMove1);
-                PUMove2 = new Sprite(resources.spritesheet.textures["PUMov2.png"]);
-                PUMove2.visible = false;
-                app.stage.addChild(PUMove2);
                 PDStatic = new Sprite(resources.spritesheet.textures["PDStatic.png"]);
+                PDStatic.scale.x = 0.3;
+                PDStatic.scale.y = 0.3;
                 PDStatic.visible = false;
                 app.stage.addChild(PDStatic);
-                PDMove1 = new Sprite(resources.spritesheet.textures["PDMov1.png"]);
-                PDMove1.visible = false;
-                app.stage.addChild(PDMove1);
-                PDMove2 = new Sprite(resources.spritesheet.textures["PDMov2.png"]);
-                PDMove2.visible = false;
-                app.stage.addChild(PDMove2);
                 TRStatic = new Sprite(resources.spritesheet.textures["TRStatic.png"]);
+                TRStatic.scale.x = 0.3;
+                TRStatic.scale.y = 0.3;
                 TRStatic.visible = false;
                 app.stage.addChild(TRStatic);
-                TRMove1 = new Sprite(resources.spritesheet.textures["TRMov1.png"]);
-                TRMove1.visible = false;
-                app.stage.addChild(TRMove1);
-                TRMove2 = new Sprite(resources.spritesheet.textures["TRMov2.png"]);
-                TRMove2.visible = false;
-                app.stage.addChild(TRMove2);
                 TLStatic = new Sprite(resources.spritesheet.textures["TLStatic.png"]);
+                TLStatic.scale.x = 0.3;
+                TLStatic.scale.y = 0.3;
                 TLStatic.visible = false;
                 app.stage.addChild(TLStatic);
-                TLMove1 = new Sprite(resources.spritesheet.textures["TLMov1.png"]);
-                TLMove1.visible = false;
-                app.stage.addChild(TLMove1);
-                TLMove2 = new Sprite(resources.spritesheet.textures["TLMov2.png"]);
-                TLMove2.visible = false;
-                app.stage.addChild(TLMove2);
                 TUStatic = new Sprite(resources.spritesheet.textures["TUStatic.png"]);
+                TUStatic.scale.x = 0.3;
+                TUStatic.scale.y = 0.3;
                 TUStatic.visible = false;
                 app.stage.addChild(TUStatic);
-                TUMove1 = new Sprite(resources.spritesheet.textures["TUMov1.png"]);
-                TUMove1.visible = false;
-                app.stage.addChild(TUMove1);
-                TUMove2 = new Sprite(resources.spritesheet.textures["TUMov2.png"]);
-                TUMove2.visible = false;
-                app.stage.addChild(TUMove2);
                 TDStatic = new Sprite(resources.spritesheet.textures["TDStatic.png"]);
+                TDStatic.scale.x = 0.3;
+                TDStatic.scale.y = 0.3;
                 TDStatic.visible = false;
                 app.stage.addChild(TDStatic);
-                TDMove1 = new Sprite(resources.spritesheet.textures["TDMov1.png"]);
-                TDMove1.visible = false;
-                app.stage.addChild(TDMove1);
-                TDMove2 = new Sprite(resources.spritesheet.textures["TDMov2.png"]);
-                TDMove2.visible = false;
-                app.stage.addChild(TDMove2);
                 Diamond = new Sprite(resources.spritesheet.textures["Diamond.png"]);
                 Diamond.visible = false;
+                Diamond.scale.x = 0.3;
+                Diamond.scale.y = 0.3;
                 app.stage.addChild(Diamond);
                 Coin = new Sprite(resources.spritesheet.textures["Coin.png"]);
+                Coin.scale.x = 0.3;
+                Coin.scale.y = 0.3;
                 Coin.visible = false;
                 app.stage.addChild(Coin);
                 Gold = new Sprite(resources.spritesheet.textures["Gold.png"]);
+                Gold.scale.x = 0.3;
+                Gold.scale.y = 0.3;
                 Gold.visible = false;
                 app.stage.addChild(Gold);
+                var frames = [];
+                frames.push(resources.spritesheet.textures["PUStatic.png"]);
+                frames.push(resources.spritesheet.textures["PUMov1.png"]);
+                frames.push(resources.spritesheet.textures["PUMov2.png"]);
+                frames.push(resources.spritesheet.textures["PUStatic.png"]);
+                PUpAnim = new PIXI.extras.AnimatedSprite(frames);
+                PUpAnim.scale.x = 0.3;
+                PUpAnim.scale.y = 0.3;
+                PUpAnim.animationSpeed = 0.2;
+                PUpAnim.play();
+                PUpAnim.visible = false;
+                app.stage.addChild(PUpAnim);
+                frames = [];
+                frames.push(resources.spritesheet.textures["PDStatic.png"]);
+                frames.push(resources.spritesheet.textures["PDMov1.png"]);
+                frames.push(resources.spritesheet.textures["PDMov2.png"]);
+                frames.push(resources.spritesheet.textures["PDStatic.png"]);
+                PDownAnim = new PIXI.extras.AnimatedSprite(frames);
+                PDownAnim.scale.x = 0.3;
+                PDownAnim.scale.y = 0.3;
+                PDownAnim.animationSpeed = 0.2;
+                PDownAnim.play();
+                PDownAnim.visible = false;
+                app.stage.addChild(PDownAnim);
+                frames = [];
+                frames.push(resources.spritesheet.textures["PRStatic.png"]);
+                frames.push(resources.spritesheet.textures["PRMov1.png"]);
+                frames.push(resources.spritesheet.textures["PRMov2.png"]);
+                frames.push(resources.spritesheet.textures["PRStatic.png"]);
+                PRightAnim = new PIXI.extras.AnimatedSprite(frames);
+                PRightAnim.scale.x = 0.3;
+                PRightAnim.scale.y = 0.3;
+                PRightAnim.animationSpeed = 0.2;
+                PRightAnim.play();
+                PRightAnim.visible = false;
+                app.stage.addChild(PRightAnim);
+                frames = [];
+                frames.push(resources.spritesheet.textures["PLStatic.png"]);
+                frames.push(resources.spritesheet.textures["PLMov1.png"]);
+                frames.push(resources.spritesheet.textures["PLMov2.png"]);
+                frames.push(resources.spritesheet.textures["PLStatic.png"]);
+                PLeftAnim = new PIXI.extras.AnimatedSprite(frames);
+                PLeftAnim.scale.x = 0.3;
+                PLeftAnim.scale.y = 0.3;
+                PLeftAnim.animationSpeed = 0.2;
+                PLeftAnim.play();
+                PLeftAnim.visible = false;
+                app.stage.addChild(PLeftAnim);
+                frames = [];
+                frames.push(resources.spritesheet.textures["TUStatic.png"]);
+                frames.push(resources.spritesheet.textures["TUMov1.png"]);
+                frames.push(resources.spritesheet.textures["TUMov2.png"]);
+                frames.push(resources.spritesheet.textures["TUStatic.png"]);
+                TUpAnim = new PIXI.extras.AnimatedSprite(frames);
+                TUpAnim.scale.x = 0.3;
+                TUpAnim.scale.y = 0.3;
+                TUpAnim.animationSpeed = 0.2;
+                TUpAnim.play();
+                TUpAnim.visible = false;
+                app.stage.addChild(TUpAnim);
+                frames = [];
+                frames.push(resources.spritesheet.textures["TDStatic.png"]);
+                frames.push(resources.spritesheet.textures["TDMov1.png"]);
+                frames.push(resources.spritesheet.textures["TDMov2.png"]);
+                frames.push(resources.spritesheet.textures["TDStatic.png"]);
+                TDownAnim = new PIXI.extras.AnimatedSprite(frames);
+                TDownAnim.scale.x = 0.3;
+                TDownAnim.scale.y = 0.3;
+                TDownAnim.animationSpeed = 0.2;
+                TDownAnim.play();
+                TDownAnim.visible = false;
+                app.stage.addChild(TDownAnim);
+                frames = [];
+                frames.push(resources.spritesheet.textures["TRStatic.png"]);
+                frames.push(resources.spritesheet.textures["TRMov1.png"]);
+                frames.push(resources.spritesheet.textures["TRMov2.png"]);
+                frames.push(resources.spritesheet.textures["TRStatic.png"]);
+                TRightAnim = new PIXI.extras.AnimatedSprite(frames);
+                TRightAnim.scale.x = 0.3;
+                TRightAnim.scale.y = 0.3;
+                TRightAnim.animationSpeed = 0.2;
+                TRightAnim.play();
+                TRightAnim.visible = false;
+                app.stage.addChild(TRightAnim);
+                frames = [];
+                frames.push(resources.spritesheet.textures["TLStatic.png"]);
+                frames.push(resources.spritesheet.textures["TLMov1.png"]);
+                frames.push(resources.spritesheet.textures["TLMov2.png"]);
+                frames.push(resources.spritesheet.textures["TLStatic.png"]);
+                TLeftAnim = new PIXI.extras.AnimatedSprite(frames);
+                TLeftAnim.scale.x = 0.3;
+                TLeftAnim.scale.y = 0.3;
+                TLeftAnim.animationSpeed = 0.2;
+                TLeftAnim.play();
+                TLeftAnim.visible = false;
+                app.stage.addChild(TLeftAnim);
+                init();
             });
     };
 
+    function init() {
+        model.thisxpos = (Math.random() * 300) + 1;
+        model.thisypos = (Math.random() * 300) + 1;
+        SpriteAct=TDStatic;
+        SpriteAct.x = model.thisxpos;
+        SpriteAct.y = model.thisypos;
+        SpriteAct.visible = true;
+        animationType = 0;
+        animationDir = "d";
+
+        function keyboard(keyCode) {
+            let key = {};
+            key.code = keyCode;
+            key.isDown = false;
+            key.isUp = true;
+            key.press = undefined;
+            key.release = undefined;
+            //The `downHandler`
+            key.downHandler = event =>{
+                if (event.keyCode === key.code) {
+                    if (key.isUp && key.press) key.press();
+                    key.isDown = true;
+                    key.isUp = false;
+                }
+                event.preventDefault();
+            };
+
+            //The `upHandler`
+            key.upHandler = event =>{
+                if (event.keyCode === key.code) {
+                    if (key.isDown && key.release) key.release();
+                    key.isDown = false;
+                    key.isUp = true;
+                }
+                event.preventDefault();
+            };
+
+            //Attach event listeners
+            window.addEventListener(
+                "keydown", key.downHandler.bind(key), false
+            );
+            window.addEventListener(
+                "keyup", key.upHandler.bind(key), false
+            );
+            return key;
+        }
+
+        //Capture the keyboard arrow keys
+        var left = keyboard(37),
+            up = keyboard(38),
+            right = keyboard(39),
+            down = keyboard(40);
+
+        //Left
+        left.press = function () {
+            model.thisvx = -5;
+            model.thisvy = 0;
+            animationDir = "l";
+            animationType = 1;
+        };
+        left.release = function () {
+            if (!right.isDown && model.thisvy === 0) {
+               model.thisvx = 0;
+            }
+            animationType = 0;
+        };
+
+        //Up
+        up.press = function () {
+            model.thisvy = -5;
+            model.thisvx = 0;
+            animationDir = "u";
+            animationType = 1;
+        };
+        up.release = function () {
+            if (!down.isDown && model.thisvx === 0) {
+                model.thisvy = 0;
+            }
+            animationType = 0;
+        };
+
+        //Right
+        right.press = function () {
+            model.thisvx = 5;
+            model.thisvy = 0;
+            animationDir = "r";
+            animationType = 1;
+        };
+        right.release = function () {
+            if (!left.isDown && model.thisvy === 0) {
+                model.thisvx = 0;
+            }
+            animationType = 0;
+        };
+
+        //Down
+        down.press = function () {
+            model.thisvy = 5;
+            model.thisvx = 0;
+            animationDir ="d";
+            animationType = 1;
+        };
+        down.release = function () {
+            if (!up.isDown && model.thisvx === 0) {
+                model.thisvy = 0;
+            }
+            animationType = 0;
+        };
+        state = play;
+
+        app.ticker.add(delta => gameLoop(delta));
+    }
+
+    function gameLoop(delta) {
+        state(delta);
+    }
+    function play() {
+        model.thisxpos += model.thisvx;
+        model.thisypos += model.thisvy;
+        SpriteAct.visible = false;
+        if(animationType == 0){
+            if(animationDir == "l"){SpriteAct = TLStatic;}
+            else if(animationDir == "u"){SpriteAct = TUStatic;}
+            else if(animationDir == "d"){SpriteAct = TDStatic;}
+            else if(animationDir == "r"){SpriteAct = TRStatic;}
+        }else{
+            if(animationDir == "l"){SpriteAct = TLeftAnim;}
+            else if(animationDir == "u"){SpriteAct = TUpAnim;}
+            else if(animationDir == "d"){SpriteAct = TDownAnim;}
+            else if(animationDir == "r"){SpriteAct = TRightAnim;}
+        }
+        SpriteAct.x = model.thisxpos;
+        SpriteAct.y = model.thisypos;
+        SpriteAct.visible = true;
+    }
+
     return {
         setupApp: setupPixiApp
-    }
-})();
+    }})();
 GameModelModule.setupApp();
